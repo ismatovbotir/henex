887786a759bcb1a1ea7a3950212f956a20d49aa5
Botir Ismatov <botirismatov@MacBook-Pro-Botir.local>
2025-12-01 20:16:56 +0500

    sefsferfsedg

---
+++ database/migrations/2025_11_27_180535_create_translations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('translations', function (Blueprint $table) {
+            $table->id();
+            $table->string('type')->default('name');
+            $table->string('lang', 2)->default('uz');
+            $table->bigInteger('model_id');
+            $table->string('model');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('translations');
+    }
+};
